9c439845698bcbc0a88b7f265137556096473099
takk2 <dudxkr94@gmail.com>
2023-03-09 00:24:33 +0900

    [Update] navigation Profile section add

--- src/components/common/navigation/styles.ts
+++ src/components/common/navigation/styles.ts
@@ -8,6 +8,7 @@ export const Wrapper = styled.aside`
   background-color: rgba(96, 142, 183, 0.2);
 
   ul {
+    margin-top: 62px;
     margin-left: 50px;
   }
   li {
@@ -17,5 +18,18 @@ export const Wrapper = styled.aside`
     font-size: ${theme.fontSize.fs40};
     font-weight: 300;
     line-height: 48px;
+  }
+`;
+
+export const ProfileWrapper = styled.div`
+  width: 100%;
+  padding: 26px;
+  border-bottom: 1px solid #bdbdbd;
+
+  div {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: #d9d9d9;
   }
 `;
